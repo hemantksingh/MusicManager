7631d05459280a5b30d294367bd1b4b209244108
hemantksingh <hemantksingh.hk@gmail.com>
2014-01-05 16:49:44 +0000

    Changed the MainViewModel to use the NotifyPropertyChangedViewModel.

--- MusicManager/MainViewModel.cs
+++ MusicManager/MainViewModel.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using MusicManager.Shared;
 using MusicManager.UI.Wpf;
 
 namespace MusicManager
 {
-    public class MainViewModel : INotifyPropertyChanged
+    public class MainViewModel : NotifyPropertyChangedViewModel
     {
         private FileSelectionViewModel _fileSelection;
         private OkCancelPanelViewModel _okCancelPanel;
@@ -52,14 +50,6 @@ namespace MusicManager
                 _fileSelection = value;
                 OnPropertyChanged();
             }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
